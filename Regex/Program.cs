75f0a8713072180d21f10e627d920ec3d74100c9
tair800 <tahiraa@code.edu.az>
2024-08-04 19:05:40 +0400

    first commit

---
+++ Regex/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+string MobilePattern = @"^\d{10}$";
+
+Console.WriteLine("Enter an email address:");
+string Email = Console.ReadLine();
+
+Console.WriteLine("Enter a mobile number:");
+string Mobile = Console.ReadLine();
+
+bool isEmailValid = Regex.IsMatch(Email, EmailPattern);
+bool isMobileValid = Regex.IsMatch(Mobile, MobilePattern);
+
+Console.WriteLine($"Valid email {isEmailValid}");
+Console.WriteLine($"Valid num {isMobileValid}");
